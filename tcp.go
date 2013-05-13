c649ee913caa689da71b509a9186855062cc94d0
Örjan Persson <orjan@spotify.com>
2013-05-12 21:55:38 -0700

    Expose TCP no delay

---
+++ tcp.go
@@ -0,0 +1,22 @@
+// Go binding for nanomsg
+
+package nanomsg
+
+// #include <nanomsg/tcp.h>
+import "C"
+
+// TcpNoDelay returns the current value of TCP no delay.
+func (s *Socket) TcpNoDelay() (bool, error) {
+	noDelay, err := s.getSockOptInt(C.NN_TCP, C.NN_TCP_NODELAY)
+	return noDelay != 0, err
+}
+
+// SetTcpNoDelay controls whether the operating system should delay packet
+// transmission in hopes of sending fewer packets (Nagle's algorithm).
+func (s *Socket) SetTcpNoDelay(noDelay bool) error {
+	var value int
+	if noDelay {
+		value = 1
+	}
+	return s.setSockOptInt(C.NN_TCP, C.NN_TCP_NODELAY, value)
+}

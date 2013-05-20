2d61aa108623b1ddd2f7975701384dfa31c8348a
Örjan Persson <orjan@spotify.com>
2013-05-19 22:45:27 -0700

    Added survey protocol

---
+++ survey.go
@@ -0,0 +1,55 @@
+// Go binding for nanomsg
+
+package nanomsg
+
+// #include <nanomsg/survey.h>
+import "C"
+
+import (
+	"time"
+)
+
+const (
+	SURVEYOR   = Protocol(C.NN_SURVEYOR)
+	RESPONDENT = Protocol(C.NN_RESPONDENT)
+)
+
+type SurveyorSocket struct {
+	*Socket
+}
+
+// NewSurveyorSocket creates a socket used to send the survey.
+// The survey is delivered to all the connected respondents. Once
+// the query is sent, the socket can be used to receive the
+// responses. When the survey deadline expires, receive will
+// return ETIMEDOUT error.
+func NewSurveyorSocket() (*SurveyorSocket, error) {
+	socket, err := NewSocket(AF_SP, SURVEYOR)
+	return &SurveyorSocket{socket}, err
+}
+
+// Deadline returns the deadline for the surveyor. Default value is 1 second.
+func (s *SurveyorSocket) Deadline() (time.Duration, error) {
+	deadline, err := s.Socket.GetSockOptInt(C.NN_SURVEYOR, C.NN_SURVEYOR_DEADLINE)
+	return time.Duration(deadline) * time.Millisecond, err
+}
+
+// SetDeadline specifies how long to wait for responses to the survey. Once
+// the deadline expires, receive function will return ETIMEDOUT error and
+// all subsequent responses to the survey will be silently dropped.
+func (s *SurveyorSocket) SetDeadline(deadline time.Duration) error {
+	deadlineMs := int(deadline / time.Millisecond)
+	return s.Socket.SetSockOptInt(C.NN_SURVEYOR, C.NN_SURVEYOR_DEADLINE, deadlineMs)
+}
+
+type RespondentSocket struct {
+	*Socket
+}
+
+// NewRespondentSocket creates a respondent socket used to respond to the
+// survey. Survey is received using receive function, response is sent
+// using send function. This socket can be connected to at most one peer.
+func NewRespondentSocket() (*RespondentSocket, error) {
+	socket, err := NewSocket(AF_SP, RESPONDENT)
+	return &RespondentSocket{socket}, err
+}

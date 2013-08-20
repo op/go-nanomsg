95b4bb47d5ed15c494bdd77dd65463e5af852ae4
Manuel Mendez <mmendez534@gmail.com>
2013-08-20 10:17:40 -0400

    tcp: rename functions to TCP, following style guidelines.

--- tcp.go
+++ tcp.go
@@ -5,15 +5,15 @@ package nanomsg
 // #include <nanomsg/tcp.h>
 import "C"
 
-// TcpNoDelay returns the current value of TCP no delay.
-func (s *Socket) TcpNoDelay() (bool, error) {
+// TCPNoDelay returns the current value of TCP no delay.
+func (s *Socket) TCPNoDelay() (bool, error) {
 	noDelay, err := s.SockOptInt(C.NN_TCP, C.NN_TCP_NODELAY)
 	return noDelay != 0, err
 }
 
-// SetTcpNoDelay controls whether the operating system should delay packet
+// SetTCPNoDelay controls whether the operating system should delay packet
 // transmission in hopes of sending fewer packets (Nagle's algorithm).
-func (s *Socket) SetTcpNoDelay(noDelay bool) error {
+func (s *Socket) SetTCPNoDelay(noDelay bool) error {
 	var value int
 	if noDelay {
 		value = 1
